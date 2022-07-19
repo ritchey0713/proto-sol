7f267fb6ede1389de1fb14ef406f3b47a2100059
dritchey01 <david.ritchey@ku.edu>
2022-07-18 20:51:18 -0700

    base

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public Rigidbody2D theRB;
+	public float moveSpeed;
+
+	// Start is called before the first frame update
+	void Start() {
+			
+	}
+
+	// Update is called once per frame
+	void Update() {
+		theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+	}
+}
